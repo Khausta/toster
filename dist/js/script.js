f45f25db5970019423555062cdb82bac1aafcdd6
Khausta <anna.a.khaustova@gmail.com>
2023-10-02 03:16:33 +0300

    swiper is added. make buttons

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,32 @@
 
+const swiper1 = new Swiper('.swiper1', {
+  speed: 2000,
+  autoplay: true,
+  spaceBetween: 10,
+  delay: 1000,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper1-button-next',
+    prevEl: '.swiper1-button-prev',
+  },
 
 
-const swiper = new Swiper('.swiper', {
-        loop: true,
+});
+
+const swiper = new Swiper('.swiper2', {
         speed: 2000,
         autoplay: true,
         spaceBetween: 10,
-        pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-                autoplayDisableOnInteraction: true,
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper2-button-next',
+          prevEl: '.swiper2-button-prev',
         },
 
+
       });
- 
+
+
+
                               
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,101 +1,30 @@
 
-function getTimeRemaining(endtime) {
-        var t = Date.parse(endtime) - Date.parse(new Date());
-        var seconds = Math.floor((t / 1000) % 60);
-        var minutes = Math.floor((t / 1000 / 60) % 60);
-        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-        // var days = Math.floor(t / (1000 * 60 * 60 * 24));
-        return {
-          'total': t,
-        //   'days': days,
-          'hours': hours,
-          'minutes': minutes,
-          'seconds': seconds
-        };
-      }
-      
-      function initializeClock(id, endtime) {
-        var clock = document.getElementById(id);
-        // var daysSpan = clock.querySelector('.days');
-        var hoursSpan = clock.querySelector('.hours');
-        var minutesSpan = clock.querySelector('.minutes');
-        var secondsSpan = clock.querySelector('.seconds');
-      
-        function updateClock() {
-          var t = getTimeRemaining(endtime);
-      
-        //   daysSpan.innerHTML = t.days;
-          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-      
-          if (t.total <= 0) {
-            clearInterval(timeinterval);
-          }
-        }
-      
-        updateClock();
-        var timeinterval = setInterval(updateClock, 1000);
-      }
-      
-      var deadline="January 01 2024 00:00:00 GMT+0300"; 
-      var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
-      initializeClock('countdown', deadline);
+const swiper1 = new Swiper('.swiper1', {
+  speed: 2000,
+  autoplay: true,
+  spaceBetween: 10,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper1-button-next',
+    prevEl: '.swiper1-button-prev',
+  },
 
-const swiper = new Swiper('.swiper', {
-        loop: true,
+
+});
+
+const swiper = new Swiper('.swiper2', {
         speed: 2000,
         autoplay: true,
         spaceBetween: 10,
-        pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-                autoplayDisableOnInteraction: true,
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper2-button-next',
+          prevEl: '.swiper2-button-prev',
         },
 
+
       });
-   
-const customBullets = document.querySelectorAll('.swiper-pagination-bullet');
-for (let i = 0; i < customBullets.length; i++) {
-  customBullets[i].classList.add(`swiper-pagination__img_${i + 1}`);
-}
 
-class Menu {
-        constructor (menuBlock) {
-                this.parentBlock = document.querySelector(`.${menuBlock}`);
-                this.selectBtn = this.parentBlock.querySelector('.select-btn');
-                this.options = this.parentBlock.querySelectorAll(".option");
-                this.sBtnText = this.parentBlock.querySelector(".sBtn-text");
-        }
-
-        init() {
-                this.clickBtn();
-                this.clickOption();
-        }
-
-        clickBtn() {
-                this.selectBtn.addEventListener("click", () =>
-                this.parentBlock.classList.toggle("active")
-              );    
-        }
-
-        clickOption() {
-                this.options.forEach((option) => {
-                        option.addEventListener("click", () => {
-                          let selectedOption = option.querySelector(".option-text").innerText;
-                          this.sBtnText.innerText = selectedOption;
-                      
-                          this.parentBlock.classList.remove("active");
-                        });
-                      });
-        }
-}
-
-const sizeMenu = new Menu('good__size-menu');
-sizeMenu.init();
-
-const colorMenu = new Menu('good__color-menu');
-colorMenu.init();
 
 
                               
