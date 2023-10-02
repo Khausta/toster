29d6704de18bb1c279ad39de967de529bce31a3e
Khausta <anna.a.khaustova@gmail.com>
2023-10-02 10:55:05 +0300

    v1

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,6 @@ const swiper1 = new Swiper('.swiper1', {
   speed: 2000,
   autoplay: true,
   spaceBetween: 10,
-  delay: 1000,
   // Navigation arrows
   navigation: {
     nextEl: '.swiper1-button-next',
